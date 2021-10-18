00685aaa6f5a96fb51ee915669aa6ecee5a6fd8a
Steve Potter <stephen.a.potter@sheffield.ac.uk>
2021-10-18 10:09:14 +0000

    Implemented advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
     # could be written as
     # if user&.authenticate(params[:session][:password]) 
     # user&. is same as user && user.blahblah
       reset_session
-      log_in user
-      redirect_to user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      log_in @user
+      redirect_to @user
     else
       flash.now[:danger] = 'Invalid email/password combination' # Uses flash.now instead of flash[:danger] to get rid of persistance problem
       render 'new'
@@ -18,7 +19,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
   
